c4297d228a249682ce6ac83ca205bddc9f48966f
Matt Lewis <matt@mplewis.com>
2020-04-04 22:26:21 -0600

    fix: configure jest-junit and circleci

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts'],
+  reporters: ['default', ['jest-junit', { outputDirectory: 'coverage' }]],
 };
