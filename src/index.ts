3ab3b5b6c426f5f306b7b33b35aae1794a23cfb6
Matt Lewis <matt@mplewis.com>
2020-04-06 20:42:56 -0600

    feat: list 100 messages

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,22 @@
+import { gmail_v1 as GmailV1 } from 'googleapis';
 import createClient from './auth';
 import { GmailClient } from './types';
+
+type Message = GmailV1.Schema$Message
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
 
-/**
- * Lists the labels in the user's account.
- * @param client An authorized OAuth2 client.
- */
-function listLabels(client: GmailClient): void {
-  client.users.labels.list({
-    userId: 'me',
-  }, (err, res) => {
-    if (err) return console.log(`The API returned an error: ${err}`);
-    if (!res) throw new Error();
-    const { labels } = res.data;
-    if (!labels) throw new Error();
-    if (labels.length) {
-      console.log('Labels:');
-      labels.forEach((label) => {
-        console.log(`- ${label.name}`);
-      });
-    } else {
-      console.log('No labels found.');
-    }
-    return null;
-  });
+async function getMessages(client: GmailClient): Promise<Message[]> {
+  const resp = await client.users.messages.list({ userId: 'me' });
+  const { messages } = resp.data;
+  if (!messages) return [];
+  return messages;
 }
 
 async function main(): Promise<void> {
-  const gmailClient = await createClient(SCOPES);
-  await listLabels(gmailClient);
+  const client = await createClient(SCOPES);
+  console.log(await getMessages(client));
 }
 
 main();
