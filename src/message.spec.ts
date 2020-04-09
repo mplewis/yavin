ca1c690e5f2fc1808f3e641cd26614a9e488193e
Matt Lewis <matt@mplewis.com>
2020-04-08 20:42:49 -0600

    feat: extractContent; handle multipart/alternative

--- src/message.spec.ts
+++ src/message.spec.ts
@@ -1,5 +1,5 @@
-import { selectPart } from './message';
-import { MessagePart } from './types';
+import { selectPart, extractContent } from './message';
+import { MessagePart, Message } from './types';
 
 describe('selectPart', () => {
   it('selects text parts first', () => {
@@ -32,5 +32,70 @@ describe('selectPart', () => {
       { mimeType: 'light/switch', body: { data: 'The Cheat is grounded!' } },
     ];
     expect(selectPart(parts).body?.data).toEqual('fhqwhgads');
+  });
+
+  it('recurses into multipart/alternative', () => {
+    const parts: MessagePart[] = [
+      {
+        mimeType: 'multipart/alternative',
+        parts: [
+          { mimeType: 'text/html', body: { data: "<p>I'm an HTML part!</p>" } },
+          { mimeType: 'text/plain', body: { data: "I'm a text part!" } },
+        ],
+      },
+      { mimeType: 'non/sense', body: { data: 'fhqwhgads' } },
+    ];
+    expect(selectPart(parts).body?.data).toEqual("I'm a text part!");
+  });
+});
+
+describe('extractContent', () => {
+  it('prefers body to parts', () => {
+    const message: Message = {
+      payload: {
+        body: { data: 'SSBzYWlkIGNvbWUgb24sIGZocXdoZ2Fkcw==' },
+        parts: [
+          { mimeType: 'text/html', body: { data: 'ZXZlcnlib2R5IHRvIHRoZSBsaW1pdA==' } },
+          { mimeType: 'text/plain', body: { data: 'ZXZlcnlib2R5LCBjb21lIG9uLCBmaHF3aGdhZHMh' } },
+        ],
+      },
+    };
+    expect(extractContent(message)).toEqual('I said come on, fhqwhgads');
+  });
+
+  it('uses the first part if no known types are present', () => {
+    const message: Message = {
+      payload: {
+        parts: [
+          { mimeType: 'non/sense', body: { data: 'ZXZlcnlib2R5IHRvIHRoZSBsaW1pdA==' } },
+          { mimeType: 'the/limit', body: { data: 'ZXZlcnlib2R5LCBjb21lIG9uLCBmaHF3aGdhZHMh' } },
+        ],
+      },
+    };
+    expect(extractContent(message)).toEqual('everybody to the limit');
+  });
+
+  it('obeys mimetype preference when using parts', () => {
+    const message: Message = {
+      payload: {
+        parts: [
+          { mimeType: 'text/html', body: { data: 'ZXZlcnlib2R5IHRvIHRoZSBsaW1pdA==' } },
+          { mimeType: 'text/plain', body: { data: 'ZXZlcnlib2R5LCBjb21lIG9uLCBmaHF3aGdhZHMh' } },
+        ],
+      },
+    };
+    expect(extractContent(message)).toEqual('everybody, come on, fhqwhgads!');
+  });
+
+  it('returns null when no content is available', () => {
+    const message: Message = {
+      payload: {
+        parts: [
+          { mimeType: 'text/html', body: {} },
+          { mimeType: 'text/plain', body: {} },
+        ],
+      },
+    };
+    expect(extractContent(message)).toEqual(null);
   });
 });
--- src/message.ts
+++ src/message.ts
@@ -1,4 +1,5 @@
-import { MessagePart } from './types';
+import { MessagePart, Message } from './types';
+import { decode } from './util';
 
 const PREFERRED_MIMETYPES = [
   'text/plain',
@@ -14,6 +15,12 @@ const PREFERRED_MIMETYPES = [
  * @param parts The MessageParts for this message from the Gmail API
  */
 export function selectPart(parts: MessagePart[]): MessagePart {
+  // HACK: multipart-alternative contains the ACTUAL parts I want to parse out,
+  // and I'm making a lot of assumptions about how I can find those actual parts right now
+  const container = parts.find((part) => part.mimeType === 'multipart/alternative' && part.parts);
+  // HACK: ?. silences ts; ugly but i promise we already did the check in the #find
+  if (container?.parts) { return selectPart(container.parts); }
+
   let selected: MessagePart | undefined;
   PREFERRED_MIMETYPES.forEach((preferred) => {
     if (selected) return;
@@ -21,4 +28,19 @@ export function selectPart(parts: MessagePart[]): MessagePart {
   });
   if (selected) return selected;
   return parts[0];
+}
+
+/**
+ * Extract some useful text content from a message, if possible. Prefer body, then parts.
+ * @param message The message from which to extract content
+ */
+export function extractContent(message: Message): string | null {
+  const { payload } = message;
+  if (!payload) return null;
+  const bodyData = payload.body?.data;
+  if (bodyData) return decode(bodyData);
+  if (!payload.parts) return null;
+  const preferredPart = selectPart(payload.parts);
+  const partData = preferredPart.body?.data;
+  return partData ? decode(partData) : null;
 }
