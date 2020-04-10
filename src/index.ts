9e9adb32589e723a6fd0b8947bc312eebbf1685a
Matt Lewis <matt@mplewis.com>
2020-04-09 20:18:36 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { writeFile } from 'fs-extra';
 import createClient from './auth';
 import { listThreads, getThread } from './gmail_api';
-import { extractContent } from './message';
+import { headerPairsToHash } from './util';
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
 
-const COUNT = 10;
+const COUNT = 20;
 const OFFSET = 0;
 
 async function main(): Promise<void> {
@@ -18,16 +18,11 @@ async function main(): Promise<void> {
     if (!thread.id) throw new Error('thread lacks id');
     const messages = await getThread(client, thread.id);
     messages.forEach(async (message) => {
-      const content = extractContent(message);
-      if (!content) {
-        console.warn(`${message.id} has no content`);
-        writeFile(`message_${message.id}.json`, JSON.stringify(message, null, 2));
-        return;
-      }
-      writeFile(`parsed_${message.id}.txt`, content.replace(/\s\s+/g, ' ').trim());
+      if (!message.payload?.headers) return;
+      const headers = headerPairsToHash(message.payload.headers);
+      writeFile(`${message.id}_headers.json`, JSON.stringify(headers, null, 4));
     });
   });
-  console.log();
 }
 
 main();
