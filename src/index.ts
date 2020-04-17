3042374690b599d319d3520378177a6e9c63f1cc
Matt Lewis <matt@mplewis.com>
2020-04-16 20:22:31 -0600

    feat: fetch all messages from inbox

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,36 @@
-import { writeFile } from 'fs-extra';
-import createClient from './auth';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+// import Message from './entities/message';
+import { Message as GmailMessage } from './types';
 import { listThreads, getThread } from './gmail_api';
-import { headerPairsToHash } from './util';
+import createClient from './auth';
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
 
-const COUNT = 20;
-const OFFSET = 0;
-
 async function main(): Promise<void> {
   const client = await createClient(SCOPES);
   const threads = await listThreads(client);
-  console.log(threads);
-  threads.slice(OFFSET, COUNT + OFFSET).forEach(async (thread) => {
-    console.log(thread.id);
-    if (!thread.id) throw new Error('thread lacks id');
-    const messages = await getThread(client, thread.id);
-    messages.forEach(async (message) => {
-      if (!message.payload?.headers) return;
-      const headers = headerPairsToHash(message.payload.headers);
-      writeFile(`${message.id}_headers.json`, JSON.stringify(headers, null, 4));
+  const conn = await createConnection();
+
+  // HACK: This only retrieves the first page of emails
+  const messageListPromises = threads
+    .filter((thread) => thread.id) // Not sure which threads lack IDs
+    .map(async (thread) => {
+      if (!thread.id) throw new Error('thread was missing id');
+      console.log(`retrieving thread id: ${thread.id}`);
+      const messages = await getThread(client, thread.id);
+      console.log(`thread id: ${thread.id}, messages: ${messages.length}`);
+      return messages;
     });
-  });
+  const messageListList = await Promise.all(messageListPromises);
+
+  let messageList: GmailMessage[] = [];
+  messageListList.forEach((oneList) => { messageList = messageList.concat(oneList); });
+
+  console.log(messageList);
+
+  await conn.close();
 }
 
 main();
