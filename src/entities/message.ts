e31970a3c07145e984884e6a37da6940dcb7e2f9
Matt Lewis <matt@mplewis.com>
2020-04-16 20:22:18 -0600

    refactor: Message entity; no User

--- src/entities/message.ts
+++ src/entities/message.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
-export default class Email {
+export default class Message {
   @PrimaryGeneratedColumn()
   id!: number;
 
