72cb2ea954e4022b1a80f949a4de89e7ea12f448
Matt Lewis <matt@mplewis.com>
2020-04-22 21:23:06 -0600

    fix: jest ignore frontend

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/', '/frontend/'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts'],
   reporters: ['default', ['jest-junit', { outputDirectory: 'coverage' }]],
 };
