3fea76bc4d19213aa96f93706dd64b0d91c358bb
Matt Lewis <matt@mplewis.com>
2020-04-26 16:59:04 -0600

    feat: return properly-populated responses

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,24 @@ import cors from 'cors';
 import express, { Express } from 'express';
 import { createConnection } from 'typeorm';
 import Message from './entities/message';
-import { Message as GmailMessage } from './types';
 import { extractPlaintextContent } from './message';
+import { EmailResponse } from './types';
+import { headerPairsToHash } from './util';
 
 const DEFAULT_PORT = 9999;
-
-interface EmailResponse {
-  id: number;
-  gmailId: string;
-  body?: string;
-  data: GmailMessage;
-}
 const ORIGIN = 'http://localhost:8080'; // HACK: This only works with the Vue dev server for now
 
 function convertMessage(message: Message): EmailResponse {
   const { id, gmailId, data } = message;
+  const headersRaw = data.payload?.headers;
+  if (!headersRaw) throw new Error(`message lacks headers: ${message.id}`);
+  const headers = headerPairsToHash(headersRaw);
+  const from = headers.From;
+  const subject = headers.Subject;
   const body = extractPlaintextContent(data);
+
   return {
-    id, gmailId, data, body,
+    id, gmailId, data, body, from, subject, tags: ['fake-tag'], suspicion: 0.01,
   };
 }
 
