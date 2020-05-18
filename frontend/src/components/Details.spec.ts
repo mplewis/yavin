c26adf870b73626bfb6ce720afb5ea41adfa75c1
Matt Lewis <matt@mplewis.com>
2020-05-17 18:48:09 -0600

    spec: udpate details to match kw/tags changes

--- frontend/src/components/Details.spec.ts
+++ frontend/src/components/Details.spec.ts
@@ -12,11 +12,25 @@ describe('Details', () => {
           suspicion: 0.201,
           body: 'this is a fake email body!',
         },
+        keywords: {
+          virus: {
+            description: 'Concerning viruses and disease transmission',
+            keywords: ['covid19', 'flu', 'pneumonia'],
+          },
+          scarewords: {
+            description: "Intended to lower the reader's guard",
+            keywords: ['act now', 'danger', 'immediate'],
+          },
+        },
       },
     });
     expect(wrapper.html()).toMatchInlineSnapshot(`
       <div class="details">
         <h1>Details</h1>
+        <b-row>
+          <b-col cols="4" class="left">Date</b-col>
+          <b-col cols="8" class="right"></b-col>
+        </b-row>
         <b-row>
           <b-col cols="4" class="left">From</b-col>
           <b-col cols="8" class="right">scammer@spamsite.xyz</b-col>
@@ -27,16 +41,9 @@ describe('Details', () => {
         </b-row>
         <b-row class="tags">
           <b-col cols="4" class="left">Tags</b-col>
-          <b-col cols="8" class="right">
-            <ul>
-              <li>
-                virus
-              </li>
-              <li>
-                scarewords
-              </li>
-            </ul>
-          </b-col>
+          <b-col cols="8" class="right"><span><details><summary>virus</summary> <p>Concerning viruses and disease transmission</p> <p>
+                  Includes: <em>covid19, flu, pneumonia</em></p></details><details><summary>scarewords</summary> <p>Intended to lower the reader's guard</p> <p>
+                  Includes: <em>act now, danger, immediate</em></p></details></span></b-col>
         </b-row>
         <b-row class="evenRow">
           <b-col cols="4" class="left">Suspicion</b-col>
